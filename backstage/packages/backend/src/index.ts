caae662309545e716fa08f5b9d5cd2c291c50763
Scott Guymer <scott.guymer@philips.com>
2024-09-05 15:48:52 +0200

    part-1

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -50,5 +50,6 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 
 backend.start();
